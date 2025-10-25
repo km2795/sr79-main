b734e20fd120d3d8f4e3ba04a2580a14b44d15cb
km2795 <kunalkm2795@gmail.com>
2025-10-25 17:10:12 +0530

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import http, { IncomingMessage, ServerResponse } from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env["PORT"];
+
+const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end("Channel SR79 Active");
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end("Page Not Found");
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Channel SR79 Active; Port: ${port}`)
+});
