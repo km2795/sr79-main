eb06147f1bfa9ff0255828bcc9ec41d40aaf5558
km2795 <kunalkm2795@gmail.com>
2025-11-02 19:09:45 +0530

    Add mimeTypes.ts for server configurations.

---
+++ src/mimeTypes.ts
@@ -0,0 +1,16 @@
+const mimeTypes: { [k: string]: string } = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".json": "application/json",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".svg": "image/svg+xml",
+    ".woff": "font/woff",
+    ".woff2": "font/woff2",
+    ".ttf": "font/ttf",
+    ".map": "application/json",
+    "": "application/octet-stream"
+}
+
+export default mimeTypes;
