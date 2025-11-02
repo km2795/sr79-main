41bf6d45453f86d8252ee9902ca2558f167aabd2
km2795 <kunalkm2795@gmail.com>
2025-11-02 19:08:02 +0530

    Add initial React Code.

---
+++ src/swish/WelcomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/css/WelcomeScreen.css";
+
+function WelcomeScreen() {
+  return (
+    <div className="welcome-screen">
+      <div>Welcome to Swish Personal Comms</div>
+      <div>
+          <img src="./public/right.png" />
+      </div>
+    </div>
+  );
+}
+
+export default WelcomeScreen;
---
+++ src/swish/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import WelcomeScreen from "./WelcomeScreen";
+
+createRoot(
+  document.getElementById("root")
+).render(<WelcomeScreen />);
