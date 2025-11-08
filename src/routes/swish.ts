ceda7862b835605103d1743d40c670b13cc214f0
km2795 <kunalkm2795@gmail.com>
2025-11-08 19:08:00 +0530

    Modify the scheme.

--- src/routes/swish.ts
+++ src/routes/swish.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import * as ChatHandler from "../ChatHandler.ts";
 import * as UserHandler from "../UserHandler.ts";
 
 const router = express.Router();
@@ -16,10 +17,14 @@ router.post('/user', async (req, res) => {
     if (userData.id in UserHandler.USER_INDEX) {
       // If password field exists in the payload, then check for authenticity.
       if ("password" in userData) {
-        if (UserHandler.USER_INDEX[userData.id] === userData.password) {
+        if (UserHandler.USER_INDEX[userData.id]["password"] === userData.password) {
           res.json({
             status: true,
-            message: "User Authenticated."
+            message: "User Authenticated.",
+
+            // If the authType is sign-in, then send the chat
+            // history as well otherwise for sign-up empty list.
+            chatHistory: userData.authType === "sign-in" ? ChatHandler.CHAT_INDEX : []
           });
         } else {
           res.json({
@@ -32,14 +37,28 @@ router.post('/user', async (req, res) => {
         res.json({status: true});
       }
     } else {
-      UserHandler.USER_INDEX[userData.id] = userData.password;
+      // Enter new user details.
+      UserHandler.USER_INDEX[userData.id] = {
+        "password": userData.password,
+        "chatHistory": []
+      };
+
       await UserHandler.updateUserIndexFile();
       res.json({ status: true });
     }
   } catch (error) {
     console.error('Error handling user:', error);
-    res.status(400).json({ status: false });
+    res.status(400).json({status: false});
   }
 });
+
+router.post("/user/chat", async (req, res) => {
+  try {
+    const userData = req.body.body;
+  } catch (err) {
+    console.log(`Error Handling Chat Session: ${err.message}`);
+    res.status(400).json({status: false});
+  }
+})
 
 export { router };
