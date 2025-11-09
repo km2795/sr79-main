a1a1f8f8bfbce98bb478e3df2b02055a83df13f5
km2795 <kunalkm2795@gmail.com>
2025-11-09 13:06:58 +0530

    Upgrade ChatListDirectory style.

--- src/swish/ui/ChatList.js
+++ src/swish/ui/ChatList.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import AddRecipientDialog from "./AddRecipientDialog";
 import ChatListDirectoryItem from "./ChatListDirectoryItem";
 import "../assets/css/ChatList.css"
-import "../assets/css/ChatListDirectoryItem.css";
 
 function ChatList({ userName, chatHistory, screenChange, updateCurrentRecipient }) {
 
@@ -33,58 +32,58 @@ function ChatList({ userName, chatHistory, screenChange, updateCurrentRecipient 
   }
 
   return (
-      <div className="chat-list-container app-default-box-welcome-screen">
-        {/* To hold the title bar. */}
-        <div className="chat-list-container-top">
-          <div className="chat-list-container-top-banner">
+    <div className="chat-list-container">
+      {/* To hold the title bar. */}
+      <div className="chat-list-container-top">
+        <div className="chat-list-container-top-banner">
 
-            {/* User's profile info. */}
-            <div className="chat-list-user-profile">
-              {/* User's profile photo. */}
-              <img className="chat-list-user-profile-photo-icon chat-view-default-icon-sizing" src="./public/images/user.png" />
-              {/* Username is displayed here. */}
-              <p className="chat-list-banner-username">{userName}</p>
-            </div>
-
-
-            {/* Action menus. */}
-            <div className="chat-list-banner-action-menu">
-              <img className="chat-list-banner-action-menu-icon chat-list-default-icon-sizing" src="./public/images/dot-menu.png" />
-            </div>
-
-          </div>
-        </div>
-
-
-        <div className="chat-list-container-middle">
-          <div className="chat-list-directory-items">
-            {
-              chatHistory.length < 1
-                ? <div className="chat-list-directory-empty-banner">It's so quiet in here!</div>
-                : Object.keys(chatHistory).map((chatKey, index) =>
-                    <ChatListDirectoryItem
-                      key={index}
-                      recipient={chatKey}
-                      chatInfo={chatHistory[chatKey]}
-                      screenChange={screenChange}
-                      updateCurrentRecipient={updateCurrentRecipient}
-                    />
-                  )
-            }
+          {/* User's profile info. */}
+          <div className="chat-list-user-profile">
+            {/* User's profile photo. */}
+            <img className="chat-list-user-profile-photo-icon chat-view-default-icon-sizing" src="./public/images/user.png" />
+            {/* Username is displayed here. */}
+            <p className="chat-list-banner-username">{userName}</p>
           </div>
 
-          {showAddRecipientDialog ? <AddRecipientDialog
-            onAccept={(recipient) => setCurrentRecipient(recipient)}
-            onClose={() => updateAddRecipientDialogVisibility(false)} /> : null}
-        </div>
 
-        <div className="chat-list-container-bottom">
-          <div className="chat-list-add-recipient-button-container" onClick={(e) => updateAddRecipientDialogVisibility(true)}>
-            <img className="chat-list-add-recipient-button chat-list-default-icon-sizing" src="./public/images/new-message.png" />
-            New Conversation
+          {/* Action menus. */}
+          <div className="chat-list-banner-action-menu">
+            <img className="chat-list-banner-action-menu-icon chat-list-default-icon-sizing" src="./public/images/dot-menu.png" />
           </div>
+
         </div>
       </div>
+
+
+      <div className="chat-list-container-middle">
+        <div className="chat-list-directory-container">
+          {
+            chatHistory.length < 1
+              ? <div className="chat-list-directory-empty-banner">It's so quiet in here!</div>
+              : Object.keys(chatHistory).map((chatKey, index) =>
+                  <ChatListDirectoryItem
+                    key={index}
+                    recipient={chatKey}
+                    chatInfo={chatHistory[chatKey]}
+                    screenChange={screenChange}
+                    updateCurrentRecipient={updateCurrentRecipient}
+                  />
+                )
+          }
+        </div>
+
+        {showAddRecipientDialog ? <AddRecipientDialog
+          onAccept={(recipient) => setCurrentRecipient(recipient)}
+          onClose={() => updateAddRecipientDialogVisibility(false)} /> : null}
+      </div>
+
+      <div className="chat-list-container-bottom">
+        <div className="chat-list-add-recipient-button-container" onClick={(e) => updateAddRecipientDialogVisibility(true)}>
+          <img className="chat-list-add-recipient-button chat-list-default-icon-sizing" src="./public/images/new-message.png" />
+          New Conversation
+        </div>
+      </div>
+    </div>
   )
 }
 
