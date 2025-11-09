8f397e568eafa538131a5beedf7dbde70866db12
km2795 <kunalkm2795@gmail.com>
2025-11-09 12:17:28 +0530

    Add sendMessage() for sending messages to the server.

--- src/swish/ui/hooks/chat/useChatProps.js
+++ src/swish/ui/hooks/chat/useChatProps.js
@@ -1,7 +1,34 @@
-function useChatProps(setCurrentRecipient) {
+import axios from "axios";
+
+function useChatProps(authCredentials, setCurrentRecipient) {
 
   return {
-    updateCurrentRecipient: (chat) => setCurrentRecipient(chat)
+    updateCurrentRecipient: (chat) => setCurrentRecipient(chat),
+
+    /**
+     * Send the message to the server.
+     *
+     * @param message
+     */
+    sendMessage: async (message) => {
+      try {
+        console.log(message);
+        const response = await axios.post("/swish/user/chat", {
+          body: {
+            "id": authCredentials.id,
+            "password": authCredentials.password,
+            "recipient": message.recipient,
+            "message": message.message,
+            "timestamp": message.timestamp
+          }
+        });
+        console.log(response);
+        return response.data.status;
+      } catch (err) {
+        console.log(`Error sending message: ${err.message}`);
+        return false;
+      }
+    }
   }
 
 }
