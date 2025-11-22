b515bd7f489325eda4b559ced38887a81e2813a5
km2795 <kunalkm2795@gmail.com>
2025-11-23 03:08:48 +0530

    Untrack build files
    Added them previously, somehow!

--- build/swish/server/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function initSwish(): Promise<void>;
-//# sourceMappingURL=index.d.ts.map
--- build/swish/server/types/UserHandler.types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface UserIndex {
-    [userId: string]: {
-        password: string;
-    };
-}
-//# sourceMappingURL=UserHandler.types.d.ts.map
