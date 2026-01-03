52db36109b48ffe80bc4534813a1c258fa04cb99
km2795 <kunalkm2795@gmail.com>
2026-01-03 23:17:55 +0530

    Move the context providers to the index.js

--- src/swish/client/ui/index.js
+++ src/swish/client/ui/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { SocketProvider } from "./contexts/socket/SocketProvider";
+import { DataProvider } from "./contexts/data_master/DataProvider";
 
 createRoot(
   document.getElementById("root")
-).render(<App />);
+).render(
+  <SocketProvider>
+    <DataProvider>
+      <App />
+    </DataProvider>
+  </SocketProvider>
+);
